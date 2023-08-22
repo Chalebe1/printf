279b1c4ae625946fddcc95fdd13428edfaef8b4c
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-22 15:54:31 -0700

    putchar

--- _printf.c
+++ _printf.c
@@ -11,42 +11,43 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
+	char c;
+	const char *s;
 
 	va_list list;
-
 	va_start(list, format);
 
-	if(format ==NULL)
+	if(format == NULL)
 		return (-1);
 
 	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
+			putchar(*format);
 			count++;
 		}
-		else if (*format == '%' && *format++ != ' ')
+		else
 		{
-			switch (format++)
+			format++;
+			switch (*format)
 			{
 				case 'c':
-					int num = va_arg(list, int);
-					write(1, &num, 1);
+					c = va_arg(list, int);
+					putchar(c);
 					count++;
 					break;
 				case 's':
-					char *str = va_arg(list, char*);
-					int str_len = 0;
-
-					while (str[str_len] != '\0')
-						str_len++;
-
-					write(1, str, str_len);
-					count += str_len;
+					s = va_arg(list, const char *);
+					while (*s != '\0')
+					{
+						putchar(*s);
+						s++;
+						count++;
+					}
 					break;
 				case '%':
-					write(1, format, 1);
+					putchar('%');
 					count++;
 					break;
 				default:
