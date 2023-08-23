3f423bb3fabe54c4fd6cccecd1fbc0c688db2f67
chalebe <angeloebuks@gmail.com>
2023-08-23 08:18:59 -0700

    task done

--- printf.c
+++ printf.c
@@ -1,4 +1,6 @@
-:ww::ww: "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h> // Include for the write function
 
 /**
  * _printf - produces output according to a format
@@ -10,47 +12,52 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	char c;
-	const char *s;
-	va_list list;
+    va_list args;
+    va_start(args, format);
 
-	va_start(list, format);
-	while (*format != '\0')
-	{
-		if (*format != '%')
-		{
-			write(1,format,1);
-			count++;
-		}
-		else
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					c = va_arg(list, int);
-					write(1,&c,1);
-					count++;
-					break;
-				case 's':
-					s = va_arg(list, const char *);
-					while (*s != '\0')
-					{
-						write(1,s,1);
-						s++;
-						count++;
-					}
-					break;
-				case '%':
-					write(1,'%',1);
-					count++;
-					break;
-			}
-		}
-		format++;
-	}
-	va_end(list);
-	return (count);
-:wq
+    int count = 0;
 
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++; // Move past the '%'
+
+            if (*format == 'c')
+            {
+                int c = va_arg(args, int);
+                write(1, &c, 1); // Write the character directly
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *string = va_arg(args, char *);
+                while (*string != '\0')
+                {
+                    write(1, string, 1); // Write each character of the string
+                    string++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, format, 1); // Write a literal '%'
+                count++;
+            }
+            else
+            {
+                // Invalid conversion specifier, ignore
+            }
+        }
+        else
+        {
+            write(1, format, 1); // Write regular characters
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count + ; // Add 1 for the newline cha
+}
