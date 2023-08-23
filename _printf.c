71a89300e02ac91436d653123d00a86200307723
Chalebe1 <Angeloebuks@gmail.com>
2023-08-23 09:14:50 -0700

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h> // Include for the write function
+
+/**
+ * _printf - Custom printf function that supports %c, %s, and %% specifiers
+ * @format: The format string containing conversion specifiers
+ * @...: Arguments corresponding to the conversion specifiers
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            if (*format == 'c')
+            {
+                int c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *s = va_arg(args, char *);
+                while (*s != '\0')
+                {
+                    write(1, s, 1);
+                    s++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, format, 1);
+                count++;
+            }
+            else
+            {
+            
+            }
+        }
+        else
+        {
+            write(1, format, 1);
+            count++;
+        }
+
+        format++;
+    }
+    va_end(args);
+    return (count);
+}
